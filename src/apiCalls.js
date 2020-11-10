9f49513b1a9f5fce47596f3f96fd7490e122f38f
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-11-10 13:06:13 -0500

    Add function to post data

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,18 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const postData = (longURL, theTitle) => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      long_url: longURL,
+      title: theTitle
+    })
+  })
+  .then(response => response.json())
+  .catch(err => console.log('ERROR IN POST DATA')) 
+}
